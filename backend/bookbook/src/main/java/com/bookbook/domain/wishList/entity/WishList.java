c5b444053e1ce08fb0be68313d338c127f69d72a
meohin <meohin.meo@gmail.com>
2025-07-25 17:23:35 +0900

    feat: WishList 엔티티 추가

---
+++ backend/bookbook/src/main/java/com/bookbook/domain/wishList/entity/WishList.java
@@ -0,0 +1,30 @@
+package com.bookbook.domain.wishList.entity;
+
+import com.bookbook.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class WishList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Rent rent;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+}
