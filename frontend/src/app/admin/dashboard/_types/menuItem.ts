b7778c51807c3d6eab184a73b08f7645378a6102
Gooraeng <birdyoon1998@gmail.com>
2025-07-26 02:06:27 +0900

    feat: 사이드바 추가

---
+++ frontend/src/app/admin/dashboard/_types/menuItem.ts
@@ -0,0 +1,16 @@
+/**
+ * 메뉴 항목 인터페이스
+ * @interface MenuItem
+ * @property {string} id - 메뉴 항목 ID
+ * @property {string} label - 메뉴 항목 레이블
+ * @property {React.ComponentType<any>} icon - 메뉴 항목 아이콘
+ * @property {MenuItem[]} children - 하위 메뉴 항목
+ * @property {string} apiPath - API 경로
+ */
+export interface MenuItem {
+  id: string;
+  label: string;
+  icon?: React.ComponentType<any>;
+  children?: MenuItem[];
+  apiPath?: string;
+}
