12ca6ee44776e9f75ff906b89c42880dee684fc5
meohin <meohin.meo@gmail.com>
2025-07-26 16:24:41 +0900

    feat: WishListRepository에 사용자 ID로 정렬된 찜 목록 조회 메서드 추가

--- backend/bookbook/src/main/java/com/bookbook/domain/wishList/repository/WishListRepository.java
+++ backend/bookbook/src/main/java/com/bookbook/domain/wishList/repository/WishListRepository.java
@@ -4,6 +4,11 @@ import com.bookbook.domain.wishList.entity.WishList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WishListRepository extends JpaRepository<WishList, Long> {
+
+    // 수정: createDate로 필드명 변경
+    List<WishList> findByUserIdOrderByCreateDateDesc(Long userId);
 }
