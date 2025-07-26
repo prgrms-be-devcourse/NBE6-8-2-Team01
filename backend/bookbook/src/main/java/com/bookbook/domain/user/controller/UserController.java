b6fae60d048b882a37314277a76641935b17ba22
heigangbae <j56233145@gmail.com>
2025-07-26 17:12:37 +0900

    feat : 개발 환경에서 테스트 할 수 있는 개발 전용 로그인 엔드포인트 생성 및 UserController 생성

---
+++ backend/bookbook/src/main/java/com/bookbook/domain/user/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.bookbook.domain.user.controller;
+
+import com.bookbook.domain.user.dto.UserLoginRequestDto;
+import com.bookbook.domain.user.dto.UserResponseDto;
+import com.bookbook.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class UserController {
+    private final UserService userService;
+
+    @Profile("dev")
+    @PostMapping("/dev/login")
+    public ResponseEntity<?> devLogin(@RequestBody UserLoginRequestDto requestDto) {
+       Optional<UserResponseDto> userResponse = userService.authenticateDevUser(requestDto);
+        if (userResponse.isPresent()) {
+            return ResponseEntity.ok(userResponse.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("개발자 로그인 실패");
+        }
+
+    }
+}
