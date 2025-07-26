5b322d0ae69dde8a4023444985487f468eecd6f8
meohin <meohin.meo@gmail.com>
2025-07-26 16:23:24 +0900

    feat: WishListService에 사용자 ID 기반 찜 목록 조회 로직 구현

--- backend/bookbook/src/main/java/com/bookbook/domain/wishList/service/WishListService.java
+++ backend/bookbook/src/main/java/com/bookbook/domain/wishList/service/WishListService.java
@@ -1,12 +1,23 @@
 package com.bookbook.domain.wishList.service;
 
+import com.bookbook.domain.user.entity.User;
+import com.bookbook.domain.wishList.dto.WishListResponseDto;
 import com.bookbook.domain.wishList.repository.WishListRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class WishListService {
 
     private final WishListRepository wishListRepository;
+
+    public List<WishListResponseDto> getWishListByUserId(Long userId) {
+        return wishListRepository.findByUserIdOrderByCreateDateDesc(userId)
+                .stream()
+                .map(WishListResponseDto::from)
+                .toList();
+    }
 }
