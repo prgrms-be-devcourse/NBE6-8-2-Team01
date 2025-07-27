a3defdf60a99cc2f2a9e8a9031d90084ce1a29fc
yoostill <yhs000323@gmail.com>
2025-07-27 22:38:05 +0900

    feat: Jpa Auditing 활성화

--- backend/bookbook/src/main/java/com/bookbook/BookbookApplication.java
+++ backend/bookbook/src/main/java/com/bookbook/BookbookApplication.java
@@ -2,8 +2,10 @@ package com.bookbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing  // JPA Auditing 활성화 (createdAt, updatedAt 자동 처리)
 public class BookbookApplication {
 
     public static void main(String[] args) {
