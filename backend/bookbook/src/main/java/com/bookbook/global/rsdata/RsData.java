0e696b2f2dba9b62272f03f619e257f84ab8f351
YangHJ2415 <guswns2415@naver.com>
2025-07-28 13:25:44 +0900

    Rsdata 수정, GlobalExceptionHandler 수정

--- backend/bookbook/src/main/java/com/bookbook/global/rsdata/RsData.java
+++ backend/bookbook/src/main/java/com/bookbook/global/rsdata/RsData.java
@@ -1,5 +1,6 @@
 package com.bookbook.global.rsdata;
 
+import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -13,6 +14,14 @@ public class RsData<T> {
     private String resultCode;
     private String msg;
     private T data;
+    private int statusCode;
+
+    public RsData(String resultCode, String msg, T data) {
+        this.resultCode = resultCode;
+        this.msg = msg;
+        this.data = data;
+        this.statusCode = Integer.parseInt(resultCode.split("-", 2)[0]);
+    }
 
     public boolean isSuccess() {
         return resultCode.startsWith("200");
