18b90759469fba2985e4480421d25e097fae3f31
yoostill <yhs000323@gmail.com>
2025-07-28 18:05:49 +0900

    resolve: merge conflict with develop1

--- backend/bookbook/src/main/java/com/bookbook/global/security/securityConfig.java
+++ backend/bookbook/src/main/java/com/bookbook/global/security/securityConfig.java
@@ -31,9 +31,9 @@ public class securityConfig {
                                 "/api/admin/login",
                                 "/api/dev/login",
                                 "/api/social/callback",
-                                "/api/v1/users/dev/login", // 개발용 로그인 추가
                                 "/bookbook/home",
-                                "/api/v1/bookbook/home" // 메인페이지 접근 허용
+                                "/api/v1/bookbook/home", // 메인페이지 접근 허용
+                                "/api/v1/users/dev/login" // 개발용 로그인 추가
                         ).permitAll()
                         .requestMatchers("/favicon.ico").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
