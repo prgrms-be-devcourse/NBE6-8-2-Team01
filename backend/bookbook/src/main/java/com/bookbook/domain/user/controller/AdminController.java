946e6ab66223b1fda6f0f96e0b48b03fe5dbf7b4
Gooraeng <birdyoon1998@gmail.com>
2025-07-29 03:49:17 +0900

    feat: AdminController 수정 (로그인 제외 작동 확인)

--- backend/bookbook/src/main/java/com/bookbook/domain/user/controller/AdminController.java
+++ backend/bookbook/src/main/java/com/bookbook/domain/user/controller/AdminController.java
@@ -1,60 +1,104 @@
 package com.bookbook.domain.user.controller;
 
 import com.bookbook.domain.suspend.dto.request.UserSuspendRequestDto;
-import com.bookbook.domain.user.service.AdminService;
+import com.bookbook.domain.suspend.dto.response.UserResumeResponseDto;
 import com.bookbook.domain.suspend.dto.response.UserSuspendResponseDto;
 import com.bookbook.domain.suspend.entity.SuspendedUser;
+import com.bookbook.domain.suspend.service.SuspendedUserService;
+import com.bookbook.domain.user.dto.response.UserDetailResponseDto;
 import com.bookbook.domain.user.dto.UserLoginRequestDto;
 import com.bookbook.domain.user.dto.UserResponseDto;
+import com.bookbook.domain.user.dto.UserBaseDto;
 import com.bookbook.domain.user.entity.User;
+import com.bookbook.domain.user.service.AdminService;
+import com.bookbook.global.rsdata.RsData;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/admin")
+@RequestMapping("/api/v1/admin")
 @RequiredArgsConstructor
 public class AdminController {
     private final AdminService adminService;
+    private final SuspendedUserService suspendedUserService;
 
     @PostMapping("/login")
-    public ResponseEntity<UserResponseDto> adminLogin(
-            @RequestBody UserLoginRequestDto requestDto
+    public RsData<UserResponseDto> adminLogin(
+            @Valid @RequestBody UserLoginRequestDto requestDto
     ) {
         User admin = adminService.login(requestDto);
-
         UserResponseDto userResponseDto = new UserResponseDto(admin);
-        return ResponseEntity.ok(userResponseDto);
+
+        return new RsData<>(
+                "200-1",
+                "관리자 %s님이 로그인하였습니다.".formatted(admin.getUsername()),
+                userResponseDto
+        );
     }
 
     @GetMapping("/members")
-    public ResponseEntity<List<UserResponseDto>> getAllUsers() {
-        List<UserResponseDto> users = adminService.getAllUsers();
-        return ResponseEntity.ok(users);
+    public RsData<List<UserBaseDto>> getAllUsers() {
+        List<UserBaseDto> users = adminService.getAllUsers();
+
+        return new RsData<>(
+                "200-1",
+                "%d명의 유저가 이용 중입니다".formatted(users.size()),
+                users
+        );
     }
 
-    @GetMapping("/members/danger")
-    public ResponseEntity<List<UserResponseDto>> getAllDangerUsers() {
-        List<UserResponseDto> allDangerUsers = adminService.getAllDangerUsers();
-        return ResponseEntity.ok(allDangerUsers);
+    @GetMapping("/members/suspend")
+    public RsData<List<UserSuspendResponseDto>> getAllSuspendedHistory() {
+        List<UserSuspendResponseDto> suspendedHistory = suspendedUserService.getAllSuspendedHistory();
+
+        return new RsData<>(
+                "200-1",
+                null,
+                suspendedHistory
+        );
     }
 
-    @GetMapping("/members/suspended")
-    public ResponseEntity<List<UserResponseDto>> getAllSuspendedUsers() {
-        List<UserResponseDto> allSuspendedUsers = adminService.getAllSuspendedUsers();
-        return ResponseEntity.ok(allSuspendedUsers);
-    }
-
-    @PostMapping("/members/suspended")
-    public ResponseEntity<UserSuspendResponseDto> suspendUser(
+    @PutMapping("/members/suspend")
+    public RsData<UserSuspendResponseDto> suspendUser(
             @RequestBody UserSuspendRequestDto requestDto
     ) {
-        SuspendedUser suspendedUser = adminService.suspendUser(requestDto);
+        SuspendedUser suspendedUser = suspendedUserService.addUserAsSuspended(requestDto);
         UserSuspendResponseDto userSuspendResponseDto = UserSuspendResponseDto.from(suspendedUser);
-        return ResponseEntity.ok(userSuspendResponseDto);
+
+        return new RsData<>(
+                "201-1",
+                "%s님을 정지하였습니다",
+                userSuspendResponseDto
+        );
     }
 
+    @GetMapping("/members/{userId}")
+    public RsData<UserDetailResponseDto> getUserDetail(
+            @PathVariable Long userId
+    ) {
+        UserDetailResponseDto specificUserInfo = adminService.getSpecificUserInfo(userId);
 
+        return new RsData<>(
+                "200-1",
+                "%d 유저의 정보를 찾았습니다.".formatted(specificUserInfo.baseResponseDto().id()),
+                specificUserInfo
+        );
+    }
+
+    @PutMapping("/members/{userId}/resume")
+    public RsData<UserResumeResponseDto> resumeUser(
+            @PathVariable Long userId
+    ) {
+        User user = suspendedUserService.resumeUser(userId);
+        UserResumeResponseDto responseDto = UserResumeResponseDto.from(user);
+
+        return new RsData<>(
+                "200-1",
+                "%d번 유저의 정지가 해제되었습니다.".formatted(user.getId()),
+                responseDto
+        );
+    }
 }
