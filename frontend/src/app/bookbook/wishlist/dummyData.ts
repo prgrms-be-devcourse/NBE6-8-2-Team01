7ced970fe032d773113128319adfe42a0be2648d
meohin <meohin.meo@gmail.com>
2025-07-28 10:45:40 +0900

    feat: 찜 목록 테스트용 더미 데이터 추가

---
+++ frontend/src/app/bookbook/wishlist/dummyData.ts
@@ -0,0 +1,108 @@
+import { WishListItem } from './types';
+
+export const dummyWishList: WishListItem[] = [
+    {
+        id: 1,
+        rentId: 101,
+        bookTitle: '작별하지 않는다',
+        bookAuthor: '한강',
+        bookPublisher: '문학동네',
+        bookCondition: '양호',
+        rentStatus: '대여가능',
+        lenderUserId: 201,
+        lenderUserName: '김민수',
+        bookImage: '/book-sample-1.png',
+        createDate: '2024-01-15T10:30:00'
+    },
+    {
+        id: 2,
+        rentId: 102,
+        bookTitle: '내가 틀릴 수도 있습니다 숲속의 현자가 전하는 마지막 인생',
+        bookAuthor: '미욘 나티코 린데블라드',
+        bookPublisher: '다산초당',
+        bookCondition: '최상',
+        rentStatus: '대출불가',
+        lenderUserId: 202,
+        lenderUserName: '이영희',
+        bookImage: '/book-sample-2.png',
+        createDate: '2024-01-14T15:20:00'
+    },
+    {
+        id: 3,
+        rentId: 103,
+        bookTitle: '건너가는 자 익숙함에서 탁월함으로 얽매임에서 벗어남으로',
+        bookAuthor: '최진성',
+        bookPublisher: '썸앤파커스',
+        bookCondition: '양호',
+        rentStatus: '대출중',
+        lenderUserId: 203,
+        lenderUserName: '박지훈',
+        bookImage: '/book-sample-3.png',
+        createDate: '2024-01-13T09:45:00'
+    },
+    {
+        id: 4,
+        rentId: 104,
+        bookTitle: '소년이 온다',
+        bookAuthor: '한강',
+        bookPublisher: '창비',
+        bookCondition: '보통',
+        rentStatus: '대여가능',
+        lenderUserId: 204,
+        lenderUserName: '최서연',
+        bookImage: '/book-sample-4.png',
+        createDate: '2024-01-12T14:10:00'
+    },
+    {
+        id: 5,
+        rentId: 105,
+        bookTitle: '슬픔을 공부하는 슬픔',
+        bookAuthor: '신형철',
+        bookPublisher: '한겨레출판',
+        bookCondition: '최상',
+        rentStatus: '대여가능',
+        lenderUserId: 205,
+        lenderUserName: '정다은',
+        bookImage: '/book-sample-5.png',
+        createDate: '2024-01-11T11:55:00'
+    },
+    {
+        id: 6,
+        rentId: 106,
+        bookTitle: '작별하지 않는다',
+        bookAuthor: '한강',
+        bookPublisher: '문학동네',
+        bookCondition: '양호',
+        rentStatus: '대여가능',
+        lenderUserId: 206,
+        lenderUserName: '강준호',
+        bookImage: '/book-sample-1.png',
+        createDate: '2024-01-10T16:30:00'
+    },
+    {
+        id: 7,
+        rentId: 107,
+        bookTitle: '내가 틀릴 수도 있습니다 숲속의 현자가 전하는 마지막 인생',
+        bookAuthor: '미욘 나티코 린데별라드',
+        bookPublisher: '다산초당',
+        bookCondition: '최상',
+        rentStatus: '대출중',
+        lenderUserId: 207,
+        lenderUserName: '송미라',
+        bookImage: '/book-sample-2.png',
+        createDate: '2024-01-09T13:25:00'
+    },
+    {
+        id: 8,
+        rentId: 108,
+        bookTitle: '건너가는 자 익숙함에서 탁월함으로 얽매임에서 벗어남으로',
+        bookAuthor: '최진성',
+        bookPublisher: '썸앤파커스',
+        bookCondition: '양호',
+        rentStatus: '대출중',
+        lenderUserId: 208,
+        lenderUserName: '한지우',
+        bookImage: '/book-sample-3.png',
+        createDate: '2024-01-08T10:15:00'
+    }
+];
