26fef67a94c34a6eab1ddc0c2c4252ab61243196
heigangbae <j56233145@gmail.com>
2025-07-28 22:40:04 +0900

    fix : redirect-uri 변경 및 .env 파일을 읽을 수 있게 변경

--- backend/bookbook/src/main/java/com/bookbook/BookbookApplication.java
+++ backend/bookbook/src/main/java/com/bookbook/BookbookApplication.java
@@ -1,5 +1,6 @@
 package com.bookbook;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +10,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BookbookApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(BookbookApplication.class, args);
     }
 
--- backend/bookbook/src/main/java/com/bookbook/global/security/securityConfig.java
+++ backend/bookbook/src/main/java/com/bookbook/global/security/securityConfig.java
@@ -5,13 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,7 +27,7 @@ public class securityConfig {
                 .csrf(csrf -> csrf.disable()) // REST API에서는 CSRF 비활성화 (토큰 기반 인증 시)
                 .authorizeHttpRequests(authorize -> authorize
                         // 로그인 관련 경로는 모두 허용
-                        .requestMatchers("/api/admin/login", "/bookbook/users/login/dev", "/bookbook/users/social/callback", "/login/**", "/bookbook/home").permitAll()
+                        .requestMatchers("/api/admin/login", "api/v1/bookbook/users/login/dev", "api/v1/bookbook/users/social/callback", "/login/**", "/bookbook/home", "/api/v1/bookbook/login/oauth2/code/**").permitAll()
                         .requestMatchers("/favicon.ico").permitAll() // 파비콘 접근 허용
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/bookbook/rent/create").permitAll() // Rent 페이지 생성은 인증 필요, (임시)               
