cb54540a8a1537d623bdea88d9d05490ff84104a
Gooraeng <birdyoon1998@gmail.com>
2025-07-29 03:49:44 +0900

    feat: AdminService 수정

--- backend/bookbook/src/main/java/com/bookbook/domain/user/service/AdminService.java
+++ backend/bookbook/src/main/java/com/bookbook/domain/user/service/AdminService.java
@@ -1,12 +1,9 @@
 package com.bookbook.domain.user.service;
 
-import com.bookbook.domain.suspend.dto.request.UserSuspendRequestDto;
-import com.bookbook.domain.suspend.entity.SuspendedUser;
-import com.bookbook.domain.suspend.service.SuspendedUserService;
+import com.bookbook.domain.user.dto.UserBaseDto;
 import com.bookbook.domain.user.dto.UserLoginRequestDto;
-import com.bookbook.domain.user.dto.UserResponseDto;
+import com.bookbook.domain.user.dto.response.UserDetailResponseDto;
 import com.bookbook.domain.user.entity.User;
-import com.bookbook.domain.user.enums.UserStatus;
 import com.bookbook.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,15 +12,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class AdminService {
     private final UserRepository userRepository;
-    private final SuspendedUserService suspendedUserService;
     private final PasswordEncoder passwordEncoder;
-    private final Float DANGER_RATING = 1.5f;
 
     @Transactional(readOnly = true)
     public User login(UserLoginRequestDto reqBody) {
@@ -36,26 +30,21 @@ public class AdminService {
     }
 
     @Transactional(readOnly = true)
-    public List<UserResponseDto> getAllUsers() {
-        return createAllUserStream()
-                .map(UserResponseDto::new)
+    public List<UserBaseDto> getAllUsers() {
+        return userRepository.findAll().stream()
+                .map(UserBaseDto::from)
                 .toList();
     }
 
     @Transactional(readOnly = true)
-    public List<UserResponseDto> getAllDangerUsers() {
-        return createAllUserStream()
-                .filter(user -> user.getRating() <= DANGER_RATING)
-                .map(UserResponseDto::new)
-                .toList();
+    public UserDetailResponseDto getSpecificUserInfo(Long userId) {
+        User user = findByUserId(userId);
+        return UserDetailResponseDto.from(user);
     }
 
-    @Transactional(readOnly = true)
-    public List<UserResponseDto> getAllSuspendedUsers() {
-        return createAllUserStream()
-                .filter(user -> user.getUserStatus().equals(UserStatus.SUSPENDED))
-                .map(UserResponseDto::new)
-                .toList();
+    public User findByUserId(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("존재하지 않는 유저입니다."));
     }
 
     private Optional<User> findByUsername(String username) {
@@ -66,21 +55,5 @@ public class AdminService {
         if (!passwordEncoder.matches(password, user.getPassword())) {
             throw new RuntimeException("존재하지 않는 계정입니다");
         }
-    }
-
-    private Stream<User> createAllUserStream() {
-        return userRepository.findAll().stream();
-    }
-
-    private Optional<User> findUserById(Long id) {
-        return userRepository.findById(id);
-    }
-
-    @Transactional
-    public SuspendedUser suspendUser(UserSuspendRequestDto requestDto) {
-        User user = findUserById(requestDto.userId())
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 ID의 유저입니다."));
-
-        return suspendedUserService.addSuspendUser(user, requestDto);
     }
 }
