70c0a11b83c0dfb39bd62a8503981bae17f4633e
MEOHIN <96607465+MEOHIN@users.noreply.github.com>
2025-08-01 09:20:03 +0900

    refactor: header에 찜한 목록 페이지 이동 url 수정 (#122)

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -100,7 +100,7 @@ const Header = () => {
                 <Link href="/bookbook/user/notification" >
                   <Bell className="w-6 h-6 text-gray-700 hover:text-blue-600 cursor-pointer" />
                 </Link>
-                <Link href="/bookbook/wishlist" >
+                <Link href="/bookbook/user/wishlist" >
                   <Heart className="w-6 h-6 text-gray-700 hover:text-blue-600 cursor-pointer" />
                 </Link>
                 <Link href="/bookbook/user/profile" >
