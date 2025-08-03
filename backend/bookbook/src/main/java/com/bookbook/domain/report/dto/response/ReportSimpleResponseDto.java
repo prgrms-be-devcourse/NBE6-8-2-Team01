02782e82ccd5761d98d6ba2c6df5a08832dc33e1
Gooraeng <birdyoon1998@gmail.com>
2025-08-04 00:48:50 +0900

    fix: Reviewer ID 필드 삭제

--- backend/bookbook/src/main/java/com/bookbook/domain/report/dto/response/ReportSimpleResponseDto.java
+++ backend/bookbook/src/main/java/com/bookbook/domain/report/dto/response/ReportSimpleResponseDto.java
@@ -14,21 +14,15 @@ public record ReportSimpleResponseDto(
         @NonNull ReportStatus status,
         @NonNull Long reporterUserId,
         @NonNull Long targetUserId,
-        @NonNull LocalDateTime createdDate,
-        Long reviewerId
+        @NonNull LocalDateTime createdDate
 ){
     public static ReportSimpleResponseDto from(Report report){
-        Long reviewerID = report.getReviewer() == null
-                ? null
-                : report.getReviewer().getId();
-
         return ReportSimpleResponseDto.builder()
                 .id(report.getId())
                 .status(report.getStatus())
                 .reporterUserId(report.getReporterUser().getId())
                 .targetUserId(report.getTargetUser().getId())
                 .createdDate(report.getCreatedDate())
-                .reviewerId(reviewerID)
                 .build();
     }
 }
