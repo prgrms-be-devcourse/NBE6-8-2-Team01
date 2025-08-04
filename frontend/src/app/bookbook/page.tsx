fd226e1b277f0efe264c94ad3b3b90a7c7dc3c37
heigangbae <j56233145@gmail.com>
2025-08-04 21:03:59 +0900

    feat : 메인페이지에 회원 정지 모달 연결

--- frontend/src/app/bookbook/page.tsx
+++ frontend/src/app/bookbook/page.tsx
@@ -1,13 +1,28 @@
 'use client';
 
+import React from 'react';
 import HeroSection from "./mainpage/HeroSection";
-import BookRegionSection from '../components/BookRegionSection'; 
+import BookRegionSection from '../components/BookRegionSection';
+import SuspensionModal from '../components/SuspensionModal';
+import { useUserStatusCheck } from '../hooks/useUserStatusCheck';
 
 export default function Home() {
-  return (
-    <main>
-      <HeroSection />
-      <BookRegionSection />
-    </main>
-  );
+    const { isSuspended, isLoaded } = useUserStatusCheck();
+
+    if (!isLoaded) {
+        return (
+            <main>
+                <HeroSection />
+                <BookRegionSection />
+            </main>
+        );
+    }
+
+    return (
+        <main>
+            <HeroSection />
+            <BookRegionSection />
+            {isSuspended && <SuspensionModal />}
+        </main>
+    );
 }
