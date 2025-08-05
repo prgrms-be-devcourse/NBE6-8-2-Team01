4672c9623060e056dd00e69acd20f662311f6a66
Gooraeng <birdyoon1998@gmail.com>
2025-08-05 22:47:54 +0900

    docs: 서비스 주석 추가

--- backend/bookbook/src/main/java/com/bookbook/domain/report/service/ReportService.java
+++ backend/bookbook/src/main/java/com/bookbook/domain/report/service/ReportService.java
@@ -7,6 +7,7 @@ import com.bookbook.domain.report.enums.ReportStatus;
 import com.bookbook.domain.report.repository.ReportRepository;
 import com.bookbook.domain.user.entity.User;
 import com.bookbook.domain.user.repository.UserRepository;
+import com.bookbook.domain.user.service.UserService;
 import com.bookbook.global.exception.ServiceException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -22,7 +23,18 @@ public class ReportService {
 
     private final ReportRepository reportRepository;
     private final UserRepository userRepository;
+    private final UserService userService;
 
+    /**
+     * 신고글을 생성합니다.
+     *
+     * @param reporterUserId 페이지 기본 정보
+     * @param targetUserId 대여 글 상태의 리스트
+     * @param reason 신고 대상자 ID
+     * @throws ServiceException
+     * <p>(400) 신고자 본인을 신고하려고 할 때
+     * <p>(404) 신고와 관련한 유저를 찾지 못했을 때
+     */
     @Transactional
     public void createReport(Long reporterUserId, Long targetUserId, String reason) {
         if (reporterUserId.equals(targetUserId)) {
@@ -44,6 +56,15 @@ public class ReportService {
         reportRepository.save(report);
     }
 
+    /**
+     * 대여 글을 페이지로 가져옵니다.
+     *
+     * @param pageable 페이지 기본 정보
+     * @param status 대여 글 상태의 리스트
+     * @param targetUserId 신고 대상자 ID
+     * @return 생성된 신고 글 페이지 정보
+     */
+    @Transactional(readOnly = true)
     public Page<ReportSimpleResponseDto> getReportPage(
             Pageable pageable,
             List<ReportStatus> status,
@@ -54,17 +75,36 @@ public class ReportService {
                 .map(ReportSimpleResponseDto::from);
     }
 
+    /**
+     * 신고 글 하나의 상세 정보를 가져옵니다.
+     * <p>관리자가 최초로 상세 정보를 요청하면 자동으로 검토 중 상태로 변경됩니다
+     *
+     * @param reportId 신고 글 ID
+     * @return 단일 신고 글 상세 정보
+     * @throws ServiceException (404) 신고 글이 존재하지 않은 경우
+     */
     @Transactional
-    public ReportDetailResponseDto getReportDetail(Long reportId) {
+    public ReportDetailResponseDto getReportDetail(long reportId) {
         Report report = findReportById(reportId);
 
-        report.markAsReviewed();
+        if (report.getStatus() ==  ReportStatus.PENDING) {
+            report.markAsReviewed();
+        }
 
         return ReportDetailResponseDto.from(report);
     }
 
+    /**
+     * 신고 글 하나를 처리 완료 상태로 변경합니다.
+     *
+     * @param reportId 신고 글 ID
+     * @param userId 처리 완료를 진행한 자의 ID
+     * @throws ServiceException
+     * <p>(409) 처리가 완료된 신고를 다시 처리하고자 할 때
+     * <p>(422) 대기 중인 신고를 처리 완료 상태로 바꾸고자 할 때
+     */
     @Transactional
-    public void markReportAsProcessed(Long reportId, Long userId) {
+    public void markReportAsProcessed(long reportId, long userId) {
         Report report = findReportById(reportId);
 
         ReportStatus status = report.getStatus();
@@ -77,17 +117,19 @@ public class ReportService {
             throw new ServiceException("409-1", "해당 신고는 이미 처리가 완료되었습니다.");
         }
 
-        User closerAdmin = findAdminById(userId);
+        User closerAdmin = userService.getByIdOrThrow(userId);
         // 신고 이슈를 닫은 사람을 표기할 수 있도록
         report.markAsProcessed(closerAdmin);
     }
 
-    private User findAdminById(Long adminId) {
-        return userRepository.findById(adminId)
-                .orElseThrow(() -> new ServiceException("404-1", "존재하지 않는 어드민 유저입니다."));
-    }
-
-    private Report findReportById(Long reportId) {
+    /**
+     * 신고 글 하나를 리포지토리에서 검색합니다.
+     *
+     * @param reportId 신고 글 ID
+     * @throws ServiceException (404) 신고 글이 존재하지 않은 경우
+     */
+    @Transactional(readOnly = true)
+    public Report findReportById(Long reportId) {
         return reportRepository.findById(reportId)
                 .orElseThrow(() -> new ServiceException("404-1", "존재하지 않는 신고입니다."));
     }
