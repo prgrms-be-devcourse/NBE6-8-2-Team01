a320c4f231f6727d9da617ac3a05f09be7229b1f
heigangbae <113930804+HeegangBae@users.noreply.github.com>
2025-08-05 21:55:42 +0900

    [feat] 백엔드 배포 전 준비 (#188) (#195)
    * feat : 일단 빌드를 위해서 잠시  테스트에 contextLoad 삭제
    
    * feat : 도커 파일 작성
    
    * fix : 도커 파일에서 자바 버전 변경
    
    * feat : fly.io launch 실행
    
    * fix : fly deploy 안되던 문제 수정
    
    * fix : 필요없는 것들이 올라간 문제 수정
    
    * feat : db 도 서버 돌리기전 설정들 추가 및 변경

--- backend/bookbook/src/test/java/com/bookbook/BookbookApplicationTests.java
+++ backend/bookbook/src/test/java/com/bookbook/BookbookApplicationTests.java
@@ -1,13 +1,8 @@
 package com.bookbook;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookbookApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
